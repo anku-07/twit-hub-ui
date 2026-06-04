b8cab6589f1c49e02f6e29af6f1022ad4e5eaa90
anku-07 <adityamondal48765@gmail.com>
2026-06-04 18:42:41 +0530

    feat: add authentication check and loader redirection to home and profile pages

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,27 @@
 import CreatePostComponent from '@/components/CreatePostComponent/CreatePostComponent';
 import FeedComponent from '@/components/FeedComponent/FeedComponent';
 import PageWrapper from '@/layout/PageWrapper/PageWrapper';
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import Loader from '@/ui/Loader/Loder';
 
 export default function Home() {
+  const router = useRouter();
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      router.push('/login');
+    } else {
+      setIsAuthenticated(true);
+    }
+  }, [router]);
+
+  if (!isAuthenticated) {
+    return <Loader />;
+  }
+
   return (
     <PageWrapper>
       <CreatePostComponent />
--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,9 +1,27 @@
 'use client';
-
 import ProfilePage from '@/components/Profile/ProfilePage';
 import PageWrapper from '@/layout/PageWrapper/PageWrapper';
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import Loader from '@/ui/Loader/Loder';
 
 const Page = () => {
+  const router = useRouter();
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      router.push('/login');
+    } else {
+      setIsAuthenticated(true);
+    }
+  }, [router]);
+
+  if (!isAuthenticated) {
+    return <Loader />;
+  }
+
   return (
     <PageWrapper>
       <ProfilePage />
